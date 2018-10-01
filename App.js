a9a875adf391994eceb84f03abf419ba11350eff
Nishi10694 <43754330+Nishi10694@users.noreply.github.com>
2018-10-02 00:11:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
 
+import React from 'react';
+import StatefulComponent from './components/stateful-component'
+
+
+export default function APP(){
+  return(
+    <div>
+    <StatefulComponent
+    items={["Orange","apple","banana","tamale"]}
+/>
+</div>
+    )
+}
